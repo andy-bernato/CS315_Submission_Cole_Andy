902af9a62eb7067355fc2852f0edf230d6bad6e7
CBracken1 <99840791+CBracken1@users.noreply.github.com>
2023-12-07 11:49:55 -0600

    i hope this breaks nothing

--- login/login.php
+++ login/login.php
@@ -18,4 +18,31 @@
             <input type="submit"/>
         </fieldset>
     </form>
+    <?php
+    $constring = "mysql:host=localhost;port=8889;dbname=ip_projects;";
+    $user = "root";
+    $pass = "root";
+    try {
+        // creating a php database object
+        $pdo = new PDO($connString, $user, $pass);
+        // exception handling parameters
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // creating the query to get rows of data from the table
+        $sql = "SELECT * FROM student_info";
+        $result = $pdo->query($sql); // committing the query
+        // navigating through all the rows one at a time
+        if ($_SERVER["REQUEST_METHOD"] == "GET") {
+            if (isset($_GET["username"]) &&
+            isset($_GET["pass"]) && 
+            isset($_GET["email"])) {
+
+            }
+        }
+        // closing the connection object
+        $pdo = null;
+        } catch (PDOException $e) { // exception handling
+        echo "Database connection unsuccessful";
+        die($e->getMessage());
+        }
+    ?>
 </body>
