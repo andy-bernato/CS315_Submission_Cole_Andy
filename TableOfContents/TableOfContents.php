ad3450a5a880fc82a055320012df1c2ca4bd2d56
CBracken1 <99840791+CBracken1@users.noreply.github.com>
2023-12-08 16:58:12 -0600

    Table of contents dies if no login - no style yet

--- TableOfContents/TableOfContents.php
+++ TableOfContents/TableOfContents.php
@@ -7,6 +7,18 @@ session_start();
 <link rel="stylesheet" href="contentssmall.css" media="only screen and (max-width:769px)">
 <link rel="stylesheet" href="contents.css" media="only screen and (min-width:770px)">
 
+<div id="topnav">
+    <a href="../InitialForm/PageOne.php">Home</a>
+    <a href="../storePage/store.php">Store</a>
+    <a href="../returninguserlogin/returninglogin.php">Login</a>
+    <?php
+        if ($_SESSION["login"] == "true")
+        {
+            echo '<a href="../logout/logout.php">Logout</a>';
+        }
+    ?>
+</div>
+
 <div style="background-color: beige;">
     <body style="background-color: beige;">
     <?php
@@ -19,19 +31,26 @@ session_start();
     {
         echo $_SESSION["uname"] . " is currently logged in.";
     };
+    if ($_SESSION["login"] == "true")
+    {
+        echo '<menu class="vertical-menu">';
+        echo 'Table of Contents';
+        echo '<a href="../InitialForm/PageOne.php" class="active">Initial Form</a>';
+        echo '<a href="../TableAlignment/PageTwo.php">Table Alignment</a>';
+        echo '<a href="../FLASHBANG!!/PageThree.php">FLASHBANG!</a>';
+        echo '<a href="../daKing/PageFour.php">Ode to DeDeDe</a>';
+        echo '<a href="../humanpersonfanpage/theProf.php">Professor Who fanpage</a>';
+        echo '<a href="../programrap/PageSix.php">Esoteric Language Rap</a>';
+        echo '<a href="../otherassignment/PageSeven.php">Assignment Table</a>';
+        echo '<a href="../formcursion/PageEight.php">Formcursion</a>';
+        echo '<a href="../andytop10thing/tenbestthing.php">Andys Top 10 things</a>';
+        echo '<a href="../colebottom10thing/PageTen.php">Coles Bottom 10 things</a>';
+        echo '</menu>';
+    }
+    else
+    {
+        echo "Want to view the table of contents? Simply log in!";
+    }    
     ?>
-        <menu class="vertical-menu">
-            Table of Contents
-            <a href="../InitialForm/PageOne.php" class="active">Initial Form</a>
-            <a href="../TableAlignment/PageTwo.php">Table Alignment</a>
-            <a href="../FLASHBANG!!/PageThree.php">FLASHBANG!</a>
-            <a href="../daKing/PageFour.php">Ode to DeDeDe</a>
-            <a href="../humanpersonfanpage/theProf.php">Professor Who fanpage</a>
-            <a href="../programrap/PageSix.php">Esoteric Language Rap</a>
-            <a href="../otherassignment/PageSeven.php">Assignment Table</a>
-            <a href="../formcursion/PageEight.php">Formcursion</a>
-            <a href="../andytop10thing/tenbestthing.php">Andy's Top 10 things</a>
-            <a href="../colebottom10thing/PageTen.php">Cole's Bottom 10 things</a>
-        </menu>
     </body>
 </div>
