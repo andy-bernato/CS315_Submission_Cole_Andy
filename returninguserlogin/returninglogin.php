e2802b05afe7fedd7475693e3db25c945563e28d
Andy Bernatow <99744066+andy-bernato@users.noreply.github.com>
2023-12-08 18:39:06 -0600

    validateydoops

--- returninguserlogin/returninglogin.php
+++ returninguserlogin/returninglogin.php
@@ -13,6 +13,71 @@ session_start();
         }
     ?>
 </div>
+
+<?php
+    $connstring = "mysql:host=localhost;port=8889;dbname=ip_projects;";
+    $user = "root";
+    $pass = "root";
+    $uname;
+    $upass;
+    $userError = $passError = "";
+    try {
+        // creating a php database object
+        $pdo = new PDO($connstring, $user, $pass);
+        // exception handling parameters
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // echo ("here"); // Why does this appear on page load?
+
+        function clean_input($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+        
+        // navigating through all the rows one at a time
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (!(empty($_POST["User"]))) {
+                if (!(empty($_POST["Pass"]))) {
+                    $uname = clean_input($_POST["User"]);
+                    $upass = clean_input($_POST["Pass"]);
+                    if (!preg_match("/^[\.a-zA-Z0-9,!? ]*$/",$name)) {
+                        $userError = "Only letters, numbers, basic punctuation, and white space allowed";
+                    }
+
+                    $excheck = $pdo->prepare("SELECT username FROM logins WHERE username = ?");
+                    $excheck->execute([$uname]);
+                    $passcheck = $pdo->prepare("SELECT password FROM logins WHERE password = ?");
+                    $passcheck->execute([$upass]);
+                    if($excheck->rowCount() !=1) {
+                        $userError = "Username not found in database. Please double check spelling and try again";
+                    }
+                    elseif($passcheck->rowCount() !=1) {
+                        $passError = "Password is incorrect. Please double check spelling and try again";
+                    }
+                    else {
+                        $_SESSION["login"] = "true";
+                        $_SESSION["uname"] = $uname;
+                        echo "Welcome back, $uname!";
+                    }
+    
+                    
+
+                } else {
+                    $passError = "Password is required";
+                }
+            } else {
+                $userError = "Username is required.";
+            }
+        }
+        // closing the connection object
+        $pdo = null;
+        } catch (PDOException $e) { // exception handling
+        echo "Database connection unsuccessful";
+        die($e->getMessage());
+        }
+    ?>
 <link rel="stylesheet" href="newuserlogin.css" media="only screen and (min-width:770px)">
 <body id="formbody">
     <?php
@@ -40,64 +105,4 @@ session_start();
             <input type="submit" name = "submit" value = "Submit"/>
         </fieldset>
     </form>
-    <?php
-    $connstring = "mysql:host=localhost;port=8889;dbname=ip_projects;";
-    $user = "root";
-    $pass = "root";
-    $uname = $upass = "";
-    $userError = $passError = "";
-    try {
-        // creating a php database object
-        $pdo = new PDO($connstring, $user, $pass);
-        // exception handling parameters
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
-        // navigating through all the rows one at a time
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            if (isset($_POST["Username"])) {
-                if (isset($_POST["Pass"])) {
-                    $uname = clean_input($_POST["User"]);
-                    $upass = clean_input($_POST["Pass"]);
-                    if (!preg_match("/^[\.a-zA-Z0-9,!? ]*$/",$name)) {
-                        $userError = "Only letters, numbers, basic punctuation, and white space allowed";
-                    }
-
-                    $excheck = $pdo->prepare("SELECT username FROM logins WHERE username = ?");
-                    $excheck->execute([$uname]);
-                    $passcheck = $pdo->prepare("SELECT password FROM logins WHERE password = ?");
-                    $passcheck->execute([$upass]);
-                    if($excheck->rowCount() !=1) {
-                        if (!(empty($nameError)))
-                        $userError = "Username not found in database. Please double check spelling and try again";
-                    }
-                    elseif($passcheck->rowCount() !=1) {
-                        $passError = "Password is incorrect. Please double check spelling and try again";
-                    }
-                    else {
-                        $_SESSION["login"] = "true";
-                        $_SESSION["uname"] = $uname;
-                        echo "Welcome back, $uname!";
-                    }
-    
-                    function clean_input($data) {
-                        $data = trim($data);
-                        $data = stripslashes($data);
-                        $data = htmlspecialchars($data);
-                        return $data;
-                    }
-
-                } else {
-                    $passError = "Password is required";
-                }
-            } else {
-                $userError = "Username is required.";
-            }
-        }
-        // closing the connection object
-        $pdo = null;
-        } catch (PDOException $e) { // exception handling
-        echo "Database connection unsuccessful";
-        die($e->getMessage());
-        }
-    ?>
 </body>
